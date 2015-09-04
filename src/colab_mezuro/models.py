e281cc8ac11c41bec5c9f876c5fbf58acb878956
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-09-04 18:08:29 -0300

    Fixed MezuroRepository model
    It had some wrong syntax and no name attributes.
    
    Signed off by: Matheus Souza Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_mezuro/models.py
+++ src/colab_mezuro/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
 class MezuroRepository(models.Model):
-    id = models.IntegerField(pk=True)
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     scm_type = models.CharField(max_length=255)
     address = models.CharField(max_length=255)
@@ -11,3 +12,7 @@ class MezuroRepository(models.Model):
     created_at = models.DateTimeField()
     updated_at = models.DateTimeField()
     branch = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = _('Mezuro Repository')
+        verbose_name_plural = _('Mezuro Repositories')
