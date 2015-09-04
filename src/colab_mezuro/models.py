1dfd9819e13bf2d9605b30d9f429f734689f1b90
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2015-09-04 12:12:18 -0300

    Created Repository model
    Signed off by: Matheus Souza Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_mezuro/models.py
+++ src/colab_mezuro/models.py
@@ -1,3 +1,13 @@
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+class MezuroRepository(models.Model):
+    id = models.IntegerField(pk=True)
+    name = models.CharField(max_length=255)
+    scm_type = models.CharField(max_length=255)
+    address = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    license = models.CharField(max_length=255)
+    period = models.IntegerField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField()
+    branch = models.CharField(max_length=255)
