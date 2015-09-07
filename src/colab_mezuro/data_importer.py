b7c8ff74f37b501ec9360422957beb0bfd681a6f
Matheus Fernandes <matheus.souza.fernandes@gmail.com>
2015-09-07 16:10:42 -0300

    Refactor and apply PEP8 on data_importer
    Signed-off-by: Matheus Fernandes <matheus.souza.fernandes@gmail.com>

--- src/colab_mezuro/data_importer.py
+++ src/colab_mezuro/data_importer.py
@@ -1,15 +1,12 @@
 import logging
-import kalibro_client
-
-from colab.plugins.data import PluginDataImporter
 
 from .models import MezuroRepository
-
+from colab.plugins.data import PluginDataImporter
+import kalibro_client
 from kalibro_client.processor import Repository
 
 
 LOGGER = logging.getLogger('colab_mezuro')
-
 
 
 class MezuroDataImporter(PluginDataImporter):
@@ -17,25 +14,30 @@ class MezuroDataImporter(PluginDataImporter):
 
     def __init__(self, *arg, **kwargs):
         super(MezuroDataImporter, self).__init__(*arg, **kwargs)
-        kalibro_client.configure(self.config.get('extra').get('processor_address'), self.config.get('extra').get('configurations_address'))
+        kalibro_client.configure(
+            self.config.get('extra').get('processor_address'),
+            self.config.get('extra').get('configurations_address')
+        )
 
-    def get_plugin_model_instance(self, plugin_model_class, kalibro_model_instance):
+    def get_plugin_model_instance(self, plugin_model_class,
+                                  kalibro_model_instance):
         plugin_model = plugin_model_class()
 
         for attribute, value in kalibro_model_instance._asdict().iteritems():
             if hasattr(plugin_model, attribute):
                 setattr(plugin_model, attribute, value)
 
+            # These attributes are not part of _asdict()
+            plugin_model.id = kalibro_model_instance.id
+            plugin_model.created_at = kalibro_model_instance.created_at
+            plugin_model.updated_at = kalibro_model_instance.updated_at
+
         return plugin_model
+
 
 class MezuroRepositoryImporter(MezuroDataImporter):
     def fetch_data(self):
         for repository in Repository.all():
-            mezuro_repository = self.get_plugin_model_instance(MezuroRepository, repository)
-
-            # These attributes are not part of _asdict()
-            mezuro_repository.id = repository.id
-            mezuro_repository.created_at = repository.created_at
-            mezuro_repository.updated_at = repository.updated_at
-
-            mezuro_repository.save()
+            mezuro_repo = self.get_plugin_model_instance(MezuroRepository,
+                                                         repository)
+            mezuro_repo.save()
