9392f2ad13dbd162d0999cb465db76df7140124e
Alexandre Barbosa <alexandreab@live.com>
2016-01-12 16:30:23 -0200

    Add namespace to fix mezuro menu
    Signed-off-by: Alexandre Barbosa <alexandreab@live.com>
    Signed-off-by: Macartur Sousa <macartur.sc@gmail.com>

--- src/colab_mezuro/apps.py
+++ src/colab_mezuro/apps.py
@@ -4,4 +4,6 @@ from colab.plugins.utils.apps import ColabPluginAppConfig
 
 class MezuroPluginAppConfig(ColabPluginAppConfig):
     name = 'colab_mezuro'
+    namespace = 'mezuro'
+    shortname = 'mezuro'
     verbose_name = 'Mezuro Plugin'
