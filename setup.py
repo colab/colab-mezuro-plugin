b655137c939bba34e38b8d8f643baf5f8d883e56
Rafael Reggiani Manzo <rr.manzo@gmail.com>
2016-01-12 16:34:51 -0200

    Bump version

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ tests_require = [
 
 setup(
     name="colab-mezuro",
-    version='0.1.0',
+    version='0.1.1',
     author='Rafael Manzo',
     author_email='rr.manzo@gmail.com',
     url='https://github.com/colab/colab-mezuro-plugin',
